030a29b5c1da23aa5078d7a3ad9b88dc9406af9d
Aline da Cruz Rodrigues Souza <linecrsouza@gmail.com>
2016-02-26 12:29:51 -0300

    Inserindo o fetch.

--- index.php
+++ index.php
@@ -6,7 +6,13 @@ try{
     $query = "SELECT * FROM alunos";
 
     $stmt = $conexao->query($query);
-    $resultado = $stmt->fetchAll();
+    $resultado = $stmt->fetchAll(PDO::FETCH_CLASS);
+
+    foreach($resultado as $aluno){
+        echo $aluno->nome."<br>";
+    }
+
+    //print_r($resultado);
 }
 catch(\PDOException $e){
     echo "Não possível estabelecer a conexão com o banco de dados. Erro código: ".$e->getCode().": ".$e->getMessage();
